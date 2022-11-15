8783a0c7fe3fe75f6fe4a519283c53e1689d306d
pratik201289 <pratik.dbconsultant@gmail.com>
2022-11-15 11:38:28 +0000

    com-herokuapp

---
+++ com-herokuapp/src/browsertesting/FirefoxBrowserTest.java
@@ -0,0 +1,54 @@
+package browsertesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class FirefoxBrowserTest {
+    public static void main(String[] args) {
+        String baseUrl = "http://the-internet.herokuapp.com/login";
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");  //1 param key // 2 param values
+        WebDriver driver = new FirefoxDriver();
+        //launch the URL
+        driver.get(baseUrl);
+        //maximize the windows
+        driver.manage().window().maximize();
+        //we give implicit time to deliver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        //Get the title name of the page
+        String title = driver.getTitle();
+        System.out.println("Page Title is: " + title);
+
+        //Get the current URL
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+
+        //Get the page Source
+        System.out.println("Page Source: " + driver.getPageSource());
+
+        //Navigate to URL
+        //String loginURL = "https://demo.nopcommerce.com/login?returnUrl=%2F";
+        //driver.navigate().to(loginURL);
+
+
+        //find the username field element
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("Admin");
+
+        //find the password field element
+        WebElement PassowrdField = driver.findElement(By.name("password"));
+        PassowrdField.sendKeys("admin123");
+
+        //Navigate to Login Page by clicking on login link
+        // WebElement loginlink = driver.findElement(By.tagName(""));
+        //loginlink.click();
+
+        //Close the browser
+        // driver.close();
+        driver.quit();
+
+    }
+}
